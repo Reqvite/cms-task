c68123b80f9743cf5e6cc4423dd1dd4c0d639cc0
Vladyslav Yachyn <reqvite2233@gmail.com>
2023-10-25 15:50:22 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/cms-task">
+    <BrowserRouter basename="/cms-task/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
